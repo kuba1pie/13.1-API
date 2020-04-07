cfaac21fce15d127507d3c831446a7c2352b3dfc
kuba1pie <kuba1pie@gmail.com>
2020-04-07 12:12:15 +0200

    Acces Control Header

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // simple route
 app.get("/", (req, res) => {
   res.json({
